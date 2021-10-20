9314bc63134313ffff42ccd8829944983f3cabf2
novagc <novagc@yandex.ru>
2021-10-20 21:30:31 +0700

    move start logic from index.js to app.js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-const
-	express = require('express'),
-	session = require('express-session'),
-	passport = require('passport'),
-	bodyParser = require('body-parser'),
-	passportInit = require('./auth/passport'),
-	{ config, loadConfig, } = require('./config/config'),
-	app = new express();
+const { Start, } = require('./app/app');
 
-loadConfig(config).then(() => {
-	passportInit();
-
-	app.set('view engine', 'hbs');
-
-	app.use(express.static('public'));
-	app.use(bodyParser());
-	app.use(bodyParser.urlencoded());
-	app.use(session({ secret: 'ASFDJLSKDJOVN', }));
-
-	app.use(passport.initialize());
-	app.use(passport.session());
-
-	app.use(require('./routes/index'));
-	app.use(require('./routes/points'));
-	app.use(require('./routes/login'));
-
-	app.listen(config.server.port);
-});
+Start();
