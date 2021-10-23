217bb469b2852abe5e0a35fecb120995a3f32730
novagc <novagc@yandex.ru>
2021-10-24 01:26:37 +0700

    add marks type addition api

--- app/data/marks.js
+++ app/data/marks.js
@@ -1,3 +1,14 @@
 const DataController = require('./dataController'); 
 
-module.exports = new DataController(`${__dirname}/data/marks.json`, 'marks');
+let dc = new DataController(`${__dirname}/data/marks.json`, 'marks');
+
+dc.AddNewType = function(markId, type) {
+    let mark = dc.data.find(x => x.id == markId);
+    
+    if (mark) {
+        mark.types.push(type);
+        dc.Save();
+    }
+}
+
+module.exports = dc;
--- app/routes/marks.js
+++ app/routes/marks.js
@@ -1,6 +1,15 @@
 const 
     DataRouter = require('./dataRouter'),
+    check = require('../auth/check'),
     marksController = require('../data/marks'),
     marksRouter = new DataRouter(marksController);
+
+marksRouter.router.post('/marks/types/add', check, function(req, res) {
+    let markId = req.body.id;
+    let type = req.body.type;
+
+    marksController.AddNewType(markId, type);
+    res.sendStatus(200);
+});
 
 module.exports = marksRouter.router;
